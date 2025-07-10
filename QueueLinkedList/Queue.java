401f3f881aba6fdc05509a0e82fe8a2333f02d38
Dev Malu <maludev15@gmail.com>
2025-07-10 12:36:23 +0530

    [Dev Malu].Add Queue Definition and Implementation

--- QueueLinkedList/Queue.java
+++ QueueLinkedList/Queue.java
@@ -1,4 +1,47 @@
-
 public class Queue {
+	Node first = null;
+	Node temp = null;
+	int cap;
+	int limit;
+	
+	public Queue(int cap) {
+		limit = cap;
+	}
+	
+	
+	public void push(int e) {
+		Node node = new Node(e);
+		
+		if(first == null) {
+			first = node;
+			limit--;
+			temp = first;
+		}
+		
+		else {
+			temp.next = node;
+			temp = node;
+		}
+	}
+	
+	public void display() {
+		Node temp = first;
+		while(temp != null) {
+			System.out.println(temp.data);
+			temp = temp.next;
+		}
+		
+	}
+	
+	public void pop() {
+		if(first == null) {
+			System.out.println("Queue is empty!");
+		}
+		else {
+			System.out.println("first element is : " + first.data);
+			first = first.next;
+			limit++;
+		}
+	}
 
 }
---
+++ QueueLinkedList/QueueImplementation.java
@@ -0,0 +1,21 @@
+
+public class QueueImplementation {
+	public static void main(String[] args) {
+		Queue q = new Queue(10);
+		
+		q.push(1);
+		q.push(2);
+		q.push(3);
+		q.push(4);
+		q.push(5);
+		
+		
+		q.pop();
+		
+		q.pop();
+		
+		q.display();
+		
+	}
+
+}
